2bb6429ec8bb7b7d3aedbf0851187d22c48453ad
Kevin Menard <kevin@nirvdrum.com>
2022-09-21 13:26:39 -0400

    Added the framework for AST interpretation, following Chap. 7 of 'Crafting Interpreters'.

--- src/main/java/com/nirvdrum/lox/Lox.java
+++ src/main/java/com/nirvdrum/lox/Lox.java
@@ -8,29 +8,47 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
+    private enum Mode {
+        AST,
+        EVAL,
+        BOTH
+    }
 
     public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
+        if (args.length > 3) {
+            System.out.println("Usage: jlox [-m mode] [script]");
             System.exit(64);
+        } else if (args.length == 3 && args[0].equals("-m")) {
+            var mode = Mode.valueOf(args[1].toUpperCase());
+            runFile(mode, args[1]);
+        } else if (args.length == 2) {
+            var mode = Mode.valueOf(args[1].toUpperCase());
+            runPrompt(mode);
         } else if (args.length == 1) {
-            runFile(args[0]);
+            runFile(Mode.EVAL, args[0]);
         } else {
-            runPrompt();
+            runPrompt(Mode.BOTH);
         }
     }
 
-    private static void runFile(String path) throws IOException {
+    private static void runFile(Mode mode, String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
-        run(new String(bytes, Charset.defaultCharset()));
+        run(mode, new String(bytes, Charset.defaultCharset()));
 
         if (hadError) {
             System.exit(65);
         }
+
+        if (hadRuntimeError) {
+            System.exit(70);
+        }
     }
 
-    private static void runPrompt() throws IOException {
+    private static void runPrompt(Mode mode) throws IOException {
         var input = new InputStreamReader(System.in);
         var reader = new BufferedReader(input);
 
@@ -42,12 +60,12 @@ public class Lox {
                 break;
             }
 
-            run(line);
+            run(mode, line);
             hadError = false;
         }
     }
 
-    private static void run(String source) {
+    private static void run(Mode mode, String source) {
         var scanner = new Scanner(source);
         var tokens = scanner.scanTokens();
         var parser = new Parser(tokens);
@@ -58,7 +76,28 @@ public class Lox {
             return;
         }
 
-        System.out.println(new AstPrinter().print(expression));
+        switch (mode) {
+            case AST: {
+                System.out.println(new AstPrinter().print(expression));
+                return;
+            }
+
+            case EVAL: {
+                interpreter.interpret(expression);
+                return;
+            }
+
+            case BOTH: {
+                System.out.println(new AstPrinter().print(expression));
+                interpreter.interpret(expression);
+
+                return;
+            }
+
+            default: {
+                System.err.println("Unknown mode: " + mode);
+            }
+        }
     }
 
     static void error(int line, String message) {
@@ -66,7 +105,7 @@ public class Lox {
     }
 
     private static void report(int line, String where, String message) {
-        System.err.println(String.format("[line %d] Error %s: %s", line, where, message));
+        System.err.printf("[line %d] Error %s: %s%n", line, where, message);
 
         hadError = true;
     }
@@ -77,5 +116,10 @@ public class Lox {
         } else {
             report(token.line(), " at '" + token.lexeme() + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line() + "]");
+        hadRuntimeError = true;
     }
 }
